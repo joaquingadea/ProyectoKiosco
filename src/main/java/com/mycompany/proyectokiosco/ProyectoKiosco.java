8f3f58df980fce772c8046d44fea7344698cf340
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2024-08-01 12:27:32 -0300

    cambio de nombre de archivos

---
+++ src/main/java/com/mycompany/proyectokiosco/ProyectoKiosco.java
@@ -0,0 +1,7 @@
+
+package com.mycompany.proyectokiosco;
+public class ProyectoKiosco {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/main/java/com/mycompany/proyectokiosko/ProyectoKiosko.java
+++
@@ -1,7 +0,0 @@
-
-package com.mycompany.proyectokiosko;
-public class ProyectoKiosko {
-    public static void main(String[] args) {
-        
-    }
-}
